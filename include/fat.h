6842e35a8a8e9819f0f151e1ae32a6414785712b
Paulo Tobias <paulohtobias@outlook.com>
2017-12-10 22:33:02 -0200

    Adição de documentação no cabeçalho das funções

--- include/fat.h
+++ include/fat.h
@@ -25,22 +25,26 @@
 #define ATTR_DIR 1
 #define ATTR_ANY 2
 
-struct _dir_entry_t{
+/**
+ * Contém as informações sobre um diretório/arquivo.
+ */
+typedef struct dir_entry_t{
 	char filename[18];
 	uint8_t attributes;
 	uint8_t reserved[7];
 	uint16_t first_block;
 	uint32_t size;
-};
+}dir_entry_t;
 
-typedef struct _dir_entry_t dir_entry_t;
-
-union _data_cluster{
+/**
+ * Contém os dados de um arquivo/diretório.
+ * No caso de um arquivo, o dado é uma string.
+ * No caso de um diretório, são até 32 dir_entry_t (incluindo . e ..)
+ */
+typedef union data_cluster{
 	dir_entry_t dir[ENTRY_BY_CLUSTER];
 	uint8_t data[CLUSTER_SIZE];
-};
-
-typedef union _data_cluster data_cluster;
+}data_cluster;
 
 /* DATA DECLARATION */
 uint8_t boot_block[CLUSTER_SIZE];
@@ -50,22 +54,68 @@ data_cluster clusters[4086];
 dir_entry_t *g_current_dir;
 char g_current_dir_name[1024];
 
+/**
+ * Exclui todos os dados e inicializa uma nova partição vazia.
+ */
 void init(void);
 
+/**
+ * Carrega a partição para a memória. (Apenas a FAT e o Root Dir)
+ */
 void load(void);
 
+/**
+ * Finaliza o programa e atualiza o arquivo com as modificações feitas
+ * durante a execução do programa.
+ */
 void exit_and_save(void);
 
+/**
+ * Retorna o primeiro cluster livre na FAT. Usa pesquisa linear para
+ * encontrar o cluster.
+ */
 uint16_t fat_get_free_cluster(void);
 
+/**
+ * Libera todos os cluster na FAT usando first_block como cluster inicial.
+ */
 void fat_free_cluster(uint16_t first_block);
 
+/**
+ * Lê o cluster apontado pela varável block do arquivo para um data_cluster
+ * e retorna o endereço deste cluster.
+ * Importante notar que o dado será armazenado no vetor global clusters
+ * e o offset do vetor é dado por block. Portanto, o vetor é usado como
+ * uma espécie de espelho para o arquivo.
+ */
 data_cluster *read_data_cluster(uint16_t block);
 
+/**
+ * Escreve o cluster apontado pela variável block no disco. O data_cluster
+ * não precisa ser passado por parâmetro pelo fato do vetor global clusters
+ * ser um espelho do arquivo. Portanto apenas o endereço do cluster é necessário.
+ */
 void write_data_cluster(uint16_t block);
 
+/**
+ * Função de conveniência para facilitar alterar os dados de um dir_entry_t.
+ */
 void set_entry(dir_entry_t *entry, const char *filename, uint8_t attributes, uint16_t first_block, uint32_t size);
 
+/**
+ * Recebe um caminho (absoluto ou relativo) juntamente de um atributo e verifica
+ * se o arquivo/diretório existe.
+ * Em caso positivo, então a dir_entry_t com as
+ * informações sobre o arquivo/diretório é retornada. NULL é retornado em caso
+ * de falha.
+ * O parâmetro attributes serve como um filtro. Caso seja necessário que o
+ * arquivo apontado por pathname seja um pasta, como é no caso da função cd,
+ * a flag ATTR_DIR (0) deverá ser passada. Então, se em pathname for encontrado
+ * um arquivo comum, então a função deve retornar NULL. O mesmo ocorre na nas
+ * funções write, read e append, onde o dir_entry_t em pathname deve ser um arquivo.
+ * Neste caso, a flag passada é ATTR_FILE (1). Caso o atributo não seja relevante,
+ * então a flag ATTR_ANY poderá ser passada.
+ */
 dir_entry_t *search_file(const char *pathname, uint8_t attributes);
 
 #endif /* FAT_H */
--- include/shell.h
+++ include/shell.h
@@ -4,28 +4,82 @@
 #include <unistd.h>
 #include "fat.h"
 
+/**
+ * Muda o diretório corrente para pathname.
+ */
 void cd(const char *pathname);
 
+/**
+ * Mostra informações sobre um dir_entry_t que está em pathname.
+ */
 void stat(const char *pathname);
 
+/**
+ * Lista o conteúdo de um diretório.
+ */
 void ls(int arc, char **argv);
 
+/**
+ * 
+ */
 dir_entry_t *create_entry(const char *pathname, uint16_t *cluster_livre, uint8_t attribute, int recursive);
 
+/**
+ * Cria um novo diretório. Caso os diretórios parentes no caminho não
+ * existam, eles poderão ser criados automaticamente com a flag -r ou -p.
+ */
 void mkdir(int argc, char **argv);
 
+/**
+ * Cria um novo arquivo comum. Caso os diretórios parentes no caminho
+ * não existam, eles poderão ser criados automaticamente com a flag
+ * -r ou -p.
+ */
 void create_file(int argc, char **argv);
 
+/**
+ * Exclui um dir_entry_t. Caso seja um diretório, este só será apagado
+ * caso esteja vazio (. e .. não são contabilizados).
+ */
 void unlink_file(int argc, char **argv);
 
+/**
+ * Sobrescreve o conteúdo de um arquivo pela string passada. Aloca mais
+ * clusters, caso necessário.
+ */
 void write_file(int argc, char **argv);
 
+/**
+ * Escreve na saída padrão o conteúdo de um arquivo.
+ */
 void read_file(int argc, char **argv);
 
+/**
+ * Anexa a string passada ao final do arquivo. Mais clusters vão sendo
+ * alocados caso necessário.
+ */
 void append(int argc, char **argv);
 
+/**
+ * Recebe a entrada do usuário e a divide em até 3 argumentos (argv).
+ * O primeiro sempre será a função desejada e os eventuais segundo e 
+ * terceiro parâmetros variam de acordo com a função.
+ * No caso dos parâmetros adicionais, o uso de aspas simples e aspas
+ * duplas é permitido para formar strings contendo espaço e/ou aspas.
+ * Portanto, se o usuário deseja criar um diretório chamado user's home,
+ * ele poderá dar o comando mkdir "user's home" ou ainda, caso o usuário
+ * sinta a necessidade de complicar, pode até usar mkdir user"'"s' 'home.
+ * Todos serão separados em duas strings, "mkdir" e "user's home".
+ * Isto é útil para, como mostrado anteriormente, criar diretórios/arquivos
+ * com espaços no nome e, principalmente, poder escrever strings com
+ * espaço nos arquivos comuns (especialmente útil para a função append).
+ */
 char **shell_parse_command(char *command, int *argc);
 
+/**
+ * Separa o comando em (até) 3 strings usando a função shell_parse_command
+ * e chama a função correta.
+ */
 void shell_process_command(char *command);
 
 #endif /* SHELL_H */
